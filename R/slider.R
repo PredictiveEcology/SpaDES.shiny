860a2d3980d95526fdf28bfdc189ce0f92ea58ff
Mateusz Wyszyński <Mateusz@Mateuszs-MacBook-Pro.local>
2017-09-11 15:13:15 +0200

    Throw error message when necessary options are not specified for slider module.

--- R/slider.R
+++ R/slider.R
@@ -12,6 +12,16 @@
 sliderUI <- function(id, label = NULL, min, max, value) {
   ns <- NS(id)
   
+  if(missing(min)) {
+    stop(paste("Slider Module requires a minimum value."))
+  }
+  if(missing(max)) {
+    stop(paste("Slider Module requires a maximum value."))
+  }
+  if(missing(value)) {
+    stop(paste("Slider Module requires an initial value."))
+  }
+  
   tagList(
     sliderInput(ns("slider"), label = label, min, max, value)
   )
