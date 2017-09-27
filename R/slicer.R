53ce384146bf9b5a5984391fe4d1df4929e00eb8
Alex Chubaty <alex.chubaty@gmail.com>
2017-09-27 10:18:58 -0600

    cleanup and fix R CMD Check warnings

--- R/slicer.R
+++ R/slicer.R
@@ -44,7 +44,7 @@ getTableOfSubtables <- function(dataTable, categoryName) {
 #'
 #' @author Mateusz Wyszynski
 #' @export
-#' @importFrom shiny mainPanel tabPanel tabsetPanel
+#' @importFrom shiny mainPanel NS tabPanel tabsetPanel
 #' @importFrom shinydashboard box
 #' @importFrom purrr pmap map
 #' @importFrom magrittr %>%
@@ -54,7 +54,7 @@ slicerUI <- function(id, data, categoryValue, uiSequence,
                      uiFunction) {
   ns <- NS(id)
 
-  if(nrow(uiSequence) == 0) {
+  if (nrow(uiSequence) == 0) {
     uiFunction(ns, data, categoryValue)
   } else {
     category <- uiSequence$category[[1]]
@@ -70,8 +70,9 @@ slicerUI <- function(id, data, categoryValue, uiSequence,
                                  uiSequence[-1, ], uiFunction))
              }
 
-             tabPanels <-
-               pmap(subtables, function(category, dataTable) tabPanelWithSlicerContent(category, dataTable))
+             tabPanels <- pmap(subtables, function(category, dataTable) {
+               tabPanelWithSlicerContent(category, dataTable)
+              })
 
              mainPanel(
                do.call(tabsetPanel, tabPanels)
@@ -82,12 +83,15 @@ slicerUI <- function(id, data, categoryValue, uiSequence,
                shinydashboard::box(
                  width = 6,
                  background = "light-blue",
-                 slicerUI(ns(category), data = dataTable, categoryValue = category, uiSequence[-1, ], uiFunction)
+                 slicerUI(ns(category), data = dataTable, categoryValue = category,
+                          uiSequence[-1, ], uiFunction)
                )
              }
 
-             pmap(subtables, function(category, dataTable) boxWithSlicerContent(category, dataTable))
-           }
+             pmap(subtables, function(category, dataTable) {
+               boxWithSlicerContent(category, dataTable)
+             })
+          }
     )
   }
 }
@@ -98,23 +102,16 @@ slicerUI <- function(id, data, categoryValue, uiSequence,
 #'
 #' @param session  Shiny server session object.
 #'
-#' @param data Data in form of a \code{data.table}. For each row from \code{uiSequence} argument.
-#'             Both server function and ui function should receive the same data table.
-#'
-#' @param uiSequence A \code{data.table} of the form
-#'                   \code{data.table(category = list_of_categories, ui = list_of_ui_actions)}.
-#'                   Both lists should contain elements of type character.
-#'                   Both server function and ui function should receive the same data table.
-#'                   Currently there are two possible actions to perform: "tab" and "box".
-#'                   Action "box": should be used only together with \pkg{shinydashboard}.
-#'                   An example of proper \code{uiSequence} is
-#'                   \code{data.table(category = c("Alliance", "Kingdom"), ui = c("tab", "box"))}
-#'
 #' @param serverFunction A summary module server function. This function will be applied to
 #'                       extracted m-dimensional data table. Should correspond to \code{uiFunction}
 #'                       in \code{slicerUI}.
 #'
 #' @return Shiny module server function.
+#'
+#' @export
+#' @importFrom purrr pmap map
+#' @importFrom magrittr %>%
+#' @rdname slicer
 #'
 #' @examples
 #' \dontrun{
@@ -123,35 +120,35 @@ slicerUI <- function(id, data, categoryValue, uiSequence,
 #'   library(SpaDES.shiny)
 #'   library(data.table)
 #'
-#'   DT <- data.table(Alliance = c("Last Alliance of Elves and Men",
-#'                                 "Last Alliance of Elves and Men",
-#'                                 "Last Alliance of Elves and Men",
-#'                                 "Mordor",
-#'                                 "Mordor",
-#'                                 "Mordor",
-#'                                 "Saruman"),
-#'                    Race = c("Elves", "Men", "Men", "Orcs", "Orcs", "Nasguls", "Uruk-hai"),
-#'                    City = c("Rivendel", "Rohan", "Gondor", "Mordor", "Moria", "Mordor", "Isengard"),
-#'                    Forces = 22: 28)
+#'   DT <- data.table(
+#'     Alliance = c("Last Alliance of Elves and Men",
+#'                  "Last Alliance of Elves and Men",
+#'                  "Last Alliance of Elves and Men",
+#'                  "Mordor",
+#'                  "Mordor",
+#'                  "Mordor",
+#'                  "Saruman"),
+#'     Race = c("Elves", "Men", "Men", "Orcs", "Orcs", "Nasguls", "Uruk-hai"),
+#'     City = c("Rivendel", "Rohan", "Gondor", "Mordor", "Moria", "Mordor", "Isengard"),
+#'     Forces = 22: 28
+#'  )
 #'
 #'   uiSequence <- data.table(category = c("Alliance", "Race"), ui = c("tab", "box"))
 #'
-#'   server <-
-#'     function(input, output, session) {
+#'   server <- function(input, output, session) {
 #'       callModule(slicer, "slicer", DT, uiSequence,
 #'                  serverFunction = function() {
 #'                    callModule(slider, "slider")
 #'                  })
 #'     }
 #'
-#'   ui <-
-#'     dashboardPage(
+#'   ui <- dashboardPage(
 #'       dashboardHeader(),
 #'       dashboardSidebar(),
 #'       dashboardBody(
 #'         slicerUI("slicer", DT, "LOTR", uiSequence,
 #'                  uiFunction = function(ns, data, categoryValue) {
-#'                    sliderUI(ns("slider"), min = data[,min(Forces)], max = data[,sum(Forces)],
+#'                    sliderUI(ns("slider"), min = data[,min(Forces)], max = data[, sum(Forces)],
 #'                             value = data[,min(Forces)], step = 1, label = categoryValue)
 #'                  })
 #'       )
@@ -160,15 +157,10 @@ slicerUI <- function(id, data, categoryValue, uiSequence,
 #'   shinyApp(ui, server)
 #' }
 #'
-#' @export
-#' @importFrom purrr pmap map
-#' @importFrom magrittr %>%
-#' @rdname slicer
 slicer <- function(input, output, session, data, uiSequence, serverFunction) {
-  if(nrow(uiSequence) == 0) {
+  if (nrow(uiSequence) == 0) {
     serverFunction()
-  }
-  else {
+  } else {
     category <- uiSequence$category[[1]]
     subtables <- getTableOfSubtables(data, category)
 
