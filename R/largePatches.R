d3eff12ea369aaee01b7df297eaa48f276608468
Alex Chubaty <alex.chubaty@gmail.com>
2018-03-15 15:10:41 -0600

    more tweaks to largePatches

--- R/largePatches.R
+++ R/largePatches.R
@@ -61,9 +61,11 @@ largePatches <- function(session, input, output, numberOfSimulationTimes, clumpM
   #   args
   # })
 
-  clumpsReturn <- do.call(callModule, c(list(module = clumpMod2, id = "largePatches"), clumpMod2Args))
+  clumpsReturn <- reactive({
+    do.call(callModule, c(list(module = clumpMod2, id = "largePatches"), clumpMod2Args))
+  })
 
-  largePatchesData <- reactive(clumpsReturn()$Clumps)
+  largePatchesData <- reactive(clumpsReturn()()$Clumps)
 
   callModule(slicer, "slicer", data = largePatchesData,
              categoryValue = "LargePatches",
